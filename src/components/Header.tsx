2ac372b45e309fa03d9b9fa09c8ddb289fa4e2bd
Markus Wedler <markuswedler8@gmail.com>
2023-03-06 16:22:53 +1000

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,6 @@ const Header = () => {
           <li><a href="#">Главная</a></li>
           <li><a href="#plan">Программа мероприятий</a></li>
           <li><a href="#qa">Задать вопрос</a></li>
-          <li><a href="#signup">Экскурсии</a></li>
           <li><a href="#contact">Контакты</a></li>
         </ul>
       </nav>
