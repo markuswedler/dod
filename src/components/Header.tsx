7d11d2fc496c27ead3c036ba620a6c110954a864
Markus Wedler <markuswedler8@gmail.com>
2023-03-09 15:14:40 +1000

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,26 @@
 const Header = () => {
   return (
-    <header className="wrapper">
-      <a className="logo" href="/"></a>
-      <nav>
-        <ul>
-          <li><a href="#">Главная</a></li>
-          <li><a href="#plan">Программа мероприятий</a></li>
-          <li><a href="#qa">Задать вопрос</a></li>
-          <li><a href="#contact">Контакты</a></li>
-        </ul>
-      </nav>
-      <nav>
-        <ul>
-          <li><a href="/">X</a></li>
-          <li><a href="/">X</a></li>
-          <li><a href="/">X</a></li>
-          <li><a href="/">X</a></li>
-        </ul>
-      </nav>
-      <div className="menuBtn">
-        <input type="checkbox" id="menuBtn" />
-        <label htmlFor="menuBtn" />
+    <header>
+      <div className="wrapper">
+        <a className="logo" href="/"></a>
+        <div className="menuBtn">
+          <input type="checkbox" id="menuBtn" />
+          <label htmlFor="menuBtn" />
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#">Главная</a></li>
+            <li><a href="#plan">Программа мероприятий</a></li>
+            <li><a href="#qa">Задать вопрос</a></li>
+            <li><a href="#contact">Контакты</a></li>
+          </ul>
+          <ul>
+            <li><a href="/">X</a></li>
+            <li><a href="/">X</a></li>
+            <li><a href="/">X</a></li>
+            <li><a href="/">X</a></li>
+          </ul>
+        </nav>
       </div>
     </header>
   )
