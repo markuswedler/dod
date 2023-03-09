1ec5c0c7cbc259296249c4cd3c4e7aa4e2b349b3
Markus Wedler <markuswedler8@gmail.com>
2023-03-09 16:07:39 +1000

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,28 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 const Header = () => {
   const [menu, setMenu] = useState(false)
+  const menuBtn = useRef<any>()
+
+  const closeMenu = () => {
+    menuBtn.current.checked = false
+    setMenu(false)
+  }
 
   return (
     <header>
       <div className="wrapper">
         <a  className={menu? "logo black": "logo"} href="/"></a>
         <div className="menuBtn">
-          <input type="checkbox" id="menuBtn" onChange={(e)=>{
-            setMenu(e.target.checked)
-          }} />
+          <input ref={menuBtn} type="checkbox" id="menuBtn" onChange={(e)=>{setMenu(e.target.checked)}} />
           <label htmlFor="menuBtn" />
         </div>
         <nav className={menu? "": "hidden"}>
           <ul>
-            <li><a href="#">Главная</a></li>
-            <li><a href="#plan">Программа мероприятий</a></li>
-            <li><a href="#qa">Задать вопрос</a></li>
-            <li><a href="#contact">Контакты</a></li>
+            <li><a href="#" onClick={closeMenu}>Главная</a></li>
+            <li><a href="#plan" onClick={closeMenu}>Программа мероприятий</a></li>
+            <li><a href="#qa" onClick={closeMenu}>Задать вопрос</a></li>
+            <li><a href="#contact" onClick={closeMenu}>Контакты</a></li>
           </ul>
           <ul>
             <li><a href="/">X</a></li>
